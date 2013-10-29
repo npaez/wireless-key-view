f10ce7a2c2bb49ebe3565aafb2c1c5222e4d0538
Nestor Paez <nestor.2005@gmail.com>
2013-10-29 18:26:47 -0200

    Algunos bugs arreglados

--- wkv.py
+++ wkv.py
@@ -1,58 +1,72 @@
 #!/usr/bin/python
 
 # WKeyView
-# Version: ???
-# Author: un boludo
+# Version: v0.2
+# Author: nestor.2005 [at] gmail.com
+#
+# Test 123
 
 
 import os
 import sys
 
-#Comprueba si sos root
-if os.geteuid() != 0:
-    print "You must have root privileges to run this script."
-    sys.exit(1)
 
 #Asistente WKeyView
 def help():
-	print "\n\tWKeyView v?.?"
-	print "\n\tAfter type the name of the wifi, your password is in row 29. Assigned to the 'psk' (Pre-shared key)"
-	print "\tExample:\n\tpsk=testing\n\tIn this case your wifi's password is 'testing'.\n\n"
+        print "\n########################################################################################################"
+        print
+        print "[-] WKeyView v0.2"
+        print 
+        print "[-] After type the name of the wifi, your password is in row 29. Assigned to the 'psk' (Pre-shared key)"
+        print "[-] Example:\n\tpsk=testing\n\tIn this case your internet password is 'testing'.\n\n"
+        print "[-] Errors:\n\t'Error reading FILE ESSID': It means that the name is invalid or the file does not exist."
+        print "\t'Error reading MOVE. Invalid command': It means that the command that you type does not exist."
+        print 
+        print "########################################################################################################"
+
 
 #Busca los archivos de wifi guardadas y printea las wifi encontradas
 def wifi():
-
 	files = os.listdir('/etc/NetworkManager/system-connections')
-	print "\nThe wifi found are:"
+	print "\nThe found files are:"
 	for element in files:
-		print "\t~ " + element + "\n"
+		print "\t[-] " + element + "\n"
 
 
 #Lee el archivo y lo printea en pantalla
 def ReadTxt():
-
 	try:
-		text=open('/etc/NetworkManager/system-connections/' + wifi,'r')
-		line=text.readline()
+		text = open('/etc/NetworkManager/system-connections/' + NameWifi,'r')
+		line = text.readline()
 		while line != "":
 			print "\t" + line
-			line=text.readline()
+			line = text.readline()
 		text.close()
 	except:
-		print "Error reading %s ESSID"%wifi
+		print "\t[-] Error reading '%s' ESSID"%NameWifi
+		exit()
 
-	
-	
+
+
+#Comprueba si sos root
+if os.geteuid() != 0:
+    	print "You must have root privileges to run this script."
+    	sys.exit(1)	
 
 move = raw_input("To search the wifi, type 'show'. Or type 'help' for assistance.\n")
 
-if move == "help":
-	help()
+if move == 'help':
+        help()
+        wifi()
+elif move == 'show':
+        wifi()
+else:
+        print "[-] Error reading '%s'. Invalid command." %move
+        exit()
 
 
-wifi()		
-wifi = raw_input("Enter the name of the wifi:")
+NameWifi = raw_input("Enter the name of the wifi:")
 
-print "\n\t######################### " + wifi + " #########################"
+print "\n\t######################### " + NameWifi + " #########################"
 ReadTxt()
-print "\t######################### " + wifi + " #########################"
+print "\t######################### " + NameWifi + " #########################"
