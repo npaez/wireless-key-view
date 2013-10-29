847a3b7ad00ceb498365ffbd017ab7af5e4f72bc
maxicanellas <maxi.canellas@gmail.com>
2013-10-29 15:25:55 -0300

    Algunos bus arreglados

--- wkv.py
+++ wkv.py
@@ -16,6 +16,9 @@ if os.geteuid() != 0:
     print "You must have root privileges to run this script."
     sys.exit(1)
 
+PATH = '/etc/NetworkManager/system-connections'
+
+
 #Asistente WKeyView
 def help():
 	print "\n\tWKeyView v?.?"
@@ -23,30 +26,32 @@ def help():
 	print "\tExample:\n\tpsk=testing\n\tIn this case your wifi's password is 'testing'.\n\n"
 
 #Busca los archivos de wifi guardadas y printea las wifi encontradas
-def wifi():
-	files = os.listdir('/etc/NetworkManager/system-connections')
+def wifi(wifi, p):
+	files = os.listdir(p)
 	print "\nThe wifi found are:"
 	for element in files:
 		print "\t~ " + element + "\n"
 
 #Lee el archivo y lo printea en pantalla
-def ReadTxt():
-    text=open('/etc/NetworkManager/system-connections/' + wifi,'r')
-    line=text.readline()
-    while line != "":
-        print "\t" + line
-        line=text.readline()
-
-    text.close()
+def ReadTxt(p):
+    
+	try:   
+	    text=open(p + wifi,'r')
+	    line=text.readline()
+	    while line != "":
+	        print "\t" + line
+	except:
+		print "Error on seaching %s ESSID, maybe misspelled name?"%wifi
+     
 
 move = raw_input("To search the wifi, type 'show'. Or type 'help' for assistance.\n")
 
 if move == "help":
 	help()
 
-wifi()		
+wifi(wifi, PATH)		
 wifi = raw_input("Enter the name of the wifi:")
 
 print "\n\t######################### " + wifi + " #########################"
-ReadTxt()
+ReadTxt(PATH)
 print "\t######################### " + wifi + " #########################"
