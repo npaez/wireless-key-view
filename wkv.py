c405cb96e579b517a4ad6ff839a4fd3b1b2a4be0
Nestor Paez <nestor.2005@gmail.com>
2013-11-04 14:47:11 -0200

    update

--- wkv.py
+++ wkv.py
@@ -16,20 +16,21 @@ from termcolor import colored #sudo pip install termcolor
 def help():
         print
         print "########################################################################################################"
-        print ""
-        print "[-] WKeyView v0.3"
-        print "[-] For the moment, this script is only available on linux Linux. For more information contact the author."
-        print ""
-        print "[-] Example:"
+        print 
+        print "[-] WKeyView"
+        print "[-] For the moment, this script is only available on Linux. For more information contact the author."
+        print "[-] E-mail: nestor.2005 [at] gmail.com
+        print 
+        print "[-] Examples:"
         print "         - psk=testing                           (In this case your internet password is 'testing')"
         print "         - psk=PUlp__FiCt10n                     (Here, the password is 'PUlp__FiCt10n')"
         print "         - psk=HasKel_Is_n0t_aN_leNguAgE         (Password: 'HasKel_Is_n0t_aN_leNguAgE')"
-        print ""
+        print 
         print "[-] Errors:"
         print "         - 'Error reading FILE ESSID': It means that the name is invalid or the file does not exist."
         print "         - 'Error reading MOVE command': Means that the command that you typed is invalid."
         print "         - 'ImportError: No module named termcolor': This script uses the library 'termcolor'."
-        print ""
+        print 
         print "########################################################################################################"
 
 #Busca los archivos de wifi guardadas y printea las wifi encontradas
@@ -40,7 +41,6 @@ def wifi(direc):
         for element in files:
                 print "         [-] " + element + "\n"
 
-
 #Lee el archivo y lo printea en pantalla
 def ReadTxt(direc, name):
         try:
@@ -50,12 +50,11 @@ def ReadTxt(direc, name):
                         if line2.startswith("psk"):
                                 print
                                 print "         ######################### " + name + " #########################"
-                                print ""
+                                print 
                                 print colored("         ESSID PASSWORD: %s"%line2,"blue")
-                                print ""
+                                print 
                                 print "         ######################### " + name + " #########################"
                 text.close()
-
         except:
                 print colored("[-] Error reading '%s' ESSID"%name, "red")
                 exit()
@@ -81,7 +80,7 @@ def main(direc):
         elif move == 'show':
                 wifi(direc)
         else:
-                print "\n[-] Error reading '%s'. Invalid command." %move
+                print colored("\n[-] Error reading '%s'. Invalid command." %move, "red")
                 exit()
 
         NameWifi = raw_input("Enter the name of the file: ")
@@ -91,6 +90,7 @@ def main(direc):
         next(NextMove, direc)
 
 
+
 #Comprueba si sos root
 if os.geteuid() != 0:
         print "You must have root privileges to run this script."
@@ -98,4 +98,3 @@ if os.geteuid() != 0:
 
 PATH = '/etc/NetworkManager/system-connections/'
 main(PATH)
-
